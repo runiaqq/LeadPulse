c8c4aacd87e7b8b0f42c86cfbd11c8b0c6b2e6d0
docvazelin <boikovladyslav12@gmail.com>
2026-05-26 13:45:20 +0300

    pull request fix

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -33,6 +34,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'jsx-a11y': jsxA11y,
+      'react-refresh': reactRefresh,
     },
 
     settings: {
@@ -49,6 +51,7 @@ export default [
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 
